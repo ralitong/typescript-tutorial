18ff63ade847d425b764c0054b64f014c1626e98
ralitong <ral_itong@yahoo.com>
2017-09-05 20:41:14 +0800

    finished typecompatibility.spec.ts

--- src/typecompatibility.spec.ts
+++ src/typecompatibility.spec.ts
@@ -46,7 +46,7 @@ describe('Typescript type compatibility', () => {
         // y = x; // x is not compativle because it is missing the location return type
     });
 
-    it('', () => {
+    it('demonstrates the problem with rest parameters with no specific type that is used in a callback', () => {
         function invokeLater(args: any[], callback: (...args: any[]) => void) {
             /* ... Invoke callback with 'args' ... */
         }
@@ -56,9 +56,68 @@ describe('Typescript type compatibility', () => {
 
         // Confusing (x and y are actually required) and undiscoverable
         invokeLater([1, 2], (x?, y?) => console.log(x + ", " + y));
+    });
+
+    it('demonstrates how enums from one type cannot be assigned to another enum type', () => {
+        enum Status { Ready, Waiting };
+        enum Color { Red, Blue, Green };
+
+        let status = Status.Ready;
+        // status = Color.Green; not possible
+    });
+
+
+    it('demonstrates how two types with different names can be assigned with each other, static methods, properties and constructor does not matter', () => {
+        class Animal {
+            feet: number;
+            constructor(name: string, numFeet: number) { }
+        }
+
+        class Size {
+            feet: number;
+            constructor(numFeet: number) { }
+        }
+
+        let a: Animal;
+        let s: Size;
+
+        // a = s; OK: but typescript will throw a warning because s has no instance
+        // s = a; OK: but typescript will throw a warning because b has no instance
+    });
+
+    it('demonstrates how interfaces with no methods but with different types can be compatible with each other', () => {
+        interface Empty<T> {
+
+        }
+
+        let x: Empty<number>;
+        let y: Empty<string>;
+
+        // x = y; // okay, y matches structure of x. will not work though because y has no instance;
+    });
+
+    it('demonstrates that non-empty interfaces with different type implementations cannot be compatible with each other', () => {
+
+        interface NotEmpty<T> {
+            data: T;
+        }
+
+        let a: NotEmpty<number>;
+        let b: NotEmpty<string>;
+
+        // a = b; // a and b have different types so it will not work
+    });
+
+    it('demonstrates that if no type is specified in for the type parameters, the type any is used', ()=>{
+        // let identity = function<T>(x :T) : T{
+            
+        // }
+
+        // let reverse = function<U> (y : U) : U {
+        
+        // }
+
+        // identity = reverse // Okay because (x : any) => any matches (y : any) => any
     })
-
-
-
 
 })
